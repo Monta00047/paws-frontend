f4c67743bce3e828491c36a45e26929844c50c5e
Montassar-Benelkhouja <montassarbenelkhouja@takiacademyteam.com>
2025-05-26 08:54:55 +0100

    mobile menu button(feat): mobile menu button complete

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,33 +1,56 @@
 import type { FC } from 'react';
+import { useState } from 'react';
 import styles from './Header.module.scss';
 import logo from '../../assets/images/header/logo.svg';
 import menu from '../../assets/icons/menu.svg';
 import Button from '../Button/Button';
+
 const Header: FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <div className={styles.header}>
-      <img className={styles.logo} src={logo} alt="" />
-      <div className={styles.navlinks}>
-        <a className={styles.link} href="">
+      <img className={styles.logo} src={logo} alt="Logo" />
+
+      <div className={`${styles.navlinks} ${isMenuOpen ? styles.open : ''}`}>
+        <a className={styles.link} href="" onClick={closeMenu}>
           Home
         </a>
-        <a className={styles.link} href="">
+        <a className={styles.link} href="" onClick={closeMenu}>
           Services
         </a>
-        <a className={styles.link} href="">
+        <a className={styles.link} href="" onClick={closeMenu}>
           Appointment
         </a>
-        <a className={styles.link} href="">
+        <a className={styles.link} href="" onClick={closeMenu}>
           About us
         </a>
-        <a className={styles.link} href="">
+        <a className={styles.link} href="" onClick={closeMenu}>
           Blog
         </a>
       </div>
-      <Button className={styles.button} href="#" variant="tertiary">
-        Contact Us
-      </Button>
-      <img className={styles.menu} src={menu} alt="" />
+
+      <div className={`${styles.actions} ${isMenuOpen ? styles.open : ''}`}>
+        <Button className={styles.button} href="#" variant="tertiary">
+          Contact Us
+        </Button>
+      </div>
+
+      <button
+        className={styles.menuButton}
+        onClick={toggleMenu}
+        aria-label="Toggle menu"
+      >
+        <img className={styles.menu} src={menu} alt="Menu" />
+      </button>
     </div>
   );
 };
