693f66b610c37aa594572a49777188541b8b449e
Montassar-Benelkhouja <montassarbenelkhouja@takiacademyteam.com>
2025-07-10 11:36:43 +0100

    part 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
-    https: false, // Make sure this is not forcing HTTPS unless you intend to
+    https: false,
     port: 5173,
+    hmr: {
+      port: 5173,
+    },
   },
   plugins: [react()],
 });
